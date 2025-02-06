d5e24aac92555741a37087cb066b0915e46721e4
Josh Martin <joshmartin@reprogrammed.software>
2025-02-05 22:52:56 -0600

    SEO tags for an SPA

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -4,11 +4,18 @@ import ThingCard from "../../components/ThingCard/ThingCard";
 import ThingContainer from "../../components/ThingContainer/ThingContainer";
 
 import styles from './Home.module.css';
+import { Helmet } from "react-helmet-async";
 
 export function Home(props: any) {
     return (
         <div className={styles.home}>
-            {/* <h1>Home Page</h1> */}
+            <Helmet prioritizeSeoTags>
+                <title>Home - Living Things</title>
+                <meta property="og:title" content="Welcome to the Living Things site! Here, you will find information about all the living things in Josh's house."/>
+                <meta
+                    name="description"
+                    content="Welcome to the Living Things site! Here, you will find information about all the living things in Josh's house." />
+            </Helmet>
         <ThingContainer>
             {livingThingsData.map((thing) => {
                 return (
